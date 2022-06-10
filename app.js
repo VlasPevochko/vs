9f4151dcff9065a3e1f7031ad522a699181b9fe1
VlasPevochko <106087859+VlasPevochko@users.noreply.github.com>
2022-06-10 19:55:55 +0300

    начало JS

---
+++ app.js
@@ -0,0 +1,13 @@
+alert('vladick alloe')
+//camelCase
+const firstName = 'Pedik'
+const age = 29 // number
+const isProgrammer = true // boolean
+
+const $ = 'some value'
+const _ = 'private'
+
+const withNum5 = '5'
+
+const lastName = prompt('ВВЕДИ СВОЮ ФАМІЛІЮ!!')
+alert(firstName + ' ' + lastName)
