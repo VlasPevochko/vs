472fbdf0463930ad99a232fea17eda2024da5a7e
VlasPevochko <106087859+VlasPevochko@users.noreply.github.com>
2022-06-10 23:37:03 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,86 @@
-alert('vladick alloe')
-//camelCase
-const firstName = 'Pedik'
-const age = 29 // number
-const isProgrammer = true // boolean
+// alert('vladick alloe')
+// //camelCase
+// const firstName = 'Pedik'
+// // const age = 29 // number
+// const isProgrammer = true // boolean
 
-const $ = 'some value'
-const _ = 'private'
+// const $ = 'some value'
+// const _ = 'private'
 
-const withNum5 = '5'
+// const withNum5 = '5'
 
-const lastName = prompt('ВВЕДИ СВОЮ ФАМІЛІЮ!!')
-alert(firstName + ' ' + lastName)
+// const lastName = prompt('ВВЕДИ СВОЮ ФАМІЛІЮ!!')
+// alert(firstName + ' ' + lastName)
+
+//оператори
+// let currentYear = 2022
+// const birthYear = 1993
+
+// const age = currentYear + birthYear
+
+// const a = 10
+// const b = 5
+
+// let c = 32
+// c = c + a| c += a
+// c = c - a| c -= a
+
+
+// console.log(a * b)
+// console.log(a - b)
+// console.log(a + b)
+// console.log(a / b)
+
+// console.log(++currentYear)
+// console.log(--currentYear)
+
+// Типы данных
+
+// const isProgrammer = true
+// const name = 'Vseslav'
+// const age = 29
+// let x
+// null
+// console.log(typeof isProgrammer)
+
+// приоритет операторов 
+
+// const fullAge = 29
+// const birthYear = 1993
+// const currentYear = 2022
+// // >< >= <=
+// const isFullAge = currentYear - birthYear >= fullAge // 28 >= 29 => true
+// console.log(isFullAge)
+
+//условные операторы
+// const courseStatus = 'pending' // ready, fail, pending
+
+// if (courseStatus == 'ready') {
+//     console.log('курс уже готов')
+// }
+// else if (courseStatus === 'pending') {
+//     console.log('Курс пока в процессе')
+// }
+
+// const num1 = 30 // number 
+// const num2 = '30' // string
+
+//пишем ===
+
+const isReady = true
+
+// if (isReady)
+// {
+//     console.log('Все готово')
+// }
+
+// else {
+//     console.log('all good')
+// }
+
+// Тернарное выражение
+isReady ? console.log('all good') : console.log('not alright')
+
+//Булевая логика
+
+//функции
