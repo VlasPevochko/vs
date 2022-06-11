6eb59e18acbeebf77a1024c647e3f9c900b1b0a2
VlasPevochko <106087859+VlasPevochko@users.noreply.github.com>
2022-06-11 21:13:05 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 // Number
-const num = 42 //integer
-const float = 42.42 //float  
-console.log(num)
+// const num = 42 //integer
+// const float = 42.42 //float  
+// console.log(num)
 
-const pow = 10e3 // 1000 - 1 + 000
+// const pow = 10e3 // 1000 - 1 + 000
 
-console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER)
-console.log ('Math.pow 53',  Math.pow(2, 53) -1)
-console.log('MAX_VALUE', Number.MAX_VALUE)
-console.log('MIN_VALUE', Number.MIN_VALUE)
-console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY)
-console.log('NEGATIVEINFINITY', Number.NEGATIVE_INFINITY)
+// console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER)
+// console.log ('Math.pow 53',  Math.pow(2, 53) -1)
+// console.log('MAX_VALUE', Number.MAX_VALUE)
+// console.log('MIN_VALUE', Number.MIN_VALUE)
+// console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY)
+// console.log('NEGATIVEINFINITY', Number.NEGATIVE_INFINITY)
+
+// Example
+// function getRandomBetween(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min)
+// }
+
+// console.log(getRandomBetween(10,42))
+
+//Math
+
+console.log(Math.E)
+console.log(Math.PI)
+
+console.log(Math.sqrt(25))
+console.log(Math.pow(5,3))
+console.log(Math.abs(-42))
+console.log(Math.max(23,15,333,34))
+console.log(Math.floor)
